a5fb33072bcc81a89134d3e3a22f8f39ae159abb
Aamir9999 <61307008+Aamir9999@users.noreply.github.com>
2022-12-06 20:11:27 +0500

    changes

--- book/header2.php
+++ book/header2.php
@@ -20,7 +20,7 @@ $current_url = substr($_SERVER['REQUEST_URI'], 19);
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item ">
-                                <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
+                                <a class="nav-link text-white" aria-current="page" href="index.php">Home</a>
                             </li>
 
                             <li class="nav-item">
