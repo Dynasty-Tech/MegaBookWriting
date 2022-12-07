2ecac9874bafbf74c821a34ca61095cd10027211
Aamir9999 <61307008+Aamir9999@users.noreply.github.com>
2022-12-07 16:09:11 +0500

    Revert "Merge branch 'nimraBranch-6-12-22' into amirBranch-05-12-2022"
    This reverts commit b209ab40c49e7332da8d3994838c37aee3579a21, reversing
    changes made to ff998679cb8c434af072532cade65e285b2e956e.

--- book/nav.php
+++ book/nav.php
@@ -9,64 +9,47 @@ $current_url = substr($_SERVER['REQUEST_URI'], 19);
 <!-- MAIN HEADER -->
 <style>
     @media only screen and (max-width: 600px) {
-
-        .p-1,
-        .p-2,
-        .p-3,
-        .p-4,
-        .p-5,
-        .px-1,
-        .px-2,
-        .px-3,
-        .px-4,
-        .px-5 {
-            padding: 20px 0 !important;
-        }
-
-        h1 {
-            font-size: 24px !important;
-            text-decoration: underline !important;
-            font-weight: 700 !important;
-        }
-
-        h2 {
-            font-size: 18px !important;
-        }
-
-        h3 {
-            font-size: 15px !important;
-        }
-
-        h4 {
-            font-size: 14px !important;
-        }
-
-        h5 {
-            font-size: 13px !important;
-        }
-
-        h6 {
-            font-size: 16px !important;
-        }
-
-        p {
-            font-size: 14px !important;
-        }
-    }
-
-    li.nav-item a {
-        white-space: nowrap !important;
-    }
-
-    .cta-button a {
-        padding:8px 16px!important;
-        margin:0!important;
-    }
+  .p-1,
+  .p-2,
+  .p-3,
+  .p-4,
+  .p-5,
+  .px-1,
+  .px-2,
+  .px-3,
+  .px-4,
+  .px-5 {
+    padding: 20px 0 !important;
+  }
+  h1{
+    font-size:24px !important;
+    text-decoration: underline !important;
+    font-weight: 700 !important;
+  }
+  h2{
+    font-size:18px !important;
+  }
+  h3{
+    font-size:15px !important;
+  }
+  h4{
+    font-size:14px !important;
+  }
+  h5{
+    font-size:13px !important;
+  }
+  h6{
+    font-size:16px !important;
+  }
+  p{
+    font-size:14px !important;
+  }
+}
 </style>
 <section>
-    <div class="container-fluid">
+    <div class="container-fluid" >
         <div class="row">
-            <nav class="navbar navbar-expand-xxl navbar-light sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.php"><img src="assets/logo.png" /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -127,7 +110,7 @@ $current_url = substr($_SERVER['REQUEST_URI'], 19);
 
                                 </ul>
                             </li>
-
+                            
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Other Services
@@ -145,9 +128,9 @@ $current_url = substr($_SERVER['REQUEST_URI'], 19);
                             </li>
 
                         </ul>
-                        <div class="cta-button">
+                        <div>
                             <a class="btn btn-outline-dark" href="contact.php">Contact</a>
-                            <a class="btn btn-dark" href="contact.php" type="submit">Get Started</a>
+                            <button class="btn btn-dark" type="submit">Get Started</button>
                         </div>
 
                     </div>
